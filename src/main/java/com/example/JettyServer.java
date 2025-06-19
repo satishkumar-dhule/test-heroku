0d876df97fc7145b0b7d81701bef5332a0eb25cf
Satishkumar Dhule <sdhule@salesforce.com>
2025-06-19 15:19:20 +0530

    chore: clean up project structure - remove duplicates and standardize packages

--- src/main/java/com/example/JettyServer.java
+++ src/main/java/com/example/JettyServer.java
@@ -3,7 +3,6 @@ package com.example;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import com.example.demo.HelloServlet;
 
 public class JettyServer {
     public static void main(String[] args) throws Exception {
--- src/main/java/com/example/Main.java
+++
@@ -1,3 +0,0 @@
-package com.example;
-
-// This file is obsolete and intentionally left blank. Use JettyServer.java as the entry point...
